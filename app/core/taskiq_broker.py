eff4f21dcdee158c98da9e8ad7db130a0d6b333a
Макс <rumyantsev1m0s1@gmail.com>
2025-12-25 19:32:49 +0300

    dlq worker

--- app/core/taskiq_broker.py
+++ app/core/taskiq_broker.py
@@ -1,14 +1,35 @@
 from taskiq_aio_pika import AioPikaBroker
 
 # taskiq worker app.core.taskiq_broker:broker --fs-discover --tasks-pattern="app/google_mailing/send_email.py"
+# основной воркер
 broker = AioPikaBroker(
     url="amqp://guest:guest@localhost:5672",
-    queue_name="taskiq_queue",  # основная очередь
-    exchange_name="taskiq_exchange",  # основной обменник
+    queue_name="main_taskiq_queue",  # основная очередь
+    exchange_name="main_taskiq_exchange",  # основной обменник
+    dead_letter_queue_name="dlq_taskiq_queue",  # dlq очередь
+    dead_letter_exchange_name="dlq_taskiq_exchange",  # dlq обменник
+    queue_arguments={  # дополнительно для применения настроек
+        "x-dead-letter-exchange": "dlq_taskiq_exchange",
+        "x-dead-letter-routing-key": "dlq_taskiq_queue"
+    },
     declare_queues=True,  # автоматически создаем очередь и обменник
     persistent=True,  # сохраняем сообщение на диск, предотвращая утечку данных
     queue_durable=True,  # очередь выживает при перезапуске
     exchange_durable=True,  # Exchange выживает при перезапуске
     reconnect_on_fail=True,  # Автопереподключение
-    prefetch_count=20,  # ограничение параллельной обработки, предотвращая перегрузку воркеров
+    prefetch_count=20  # ограничение параллельной обработки, предотвращая перегрузку воркеров
+)
+
+# taskiq worker app.core.taskiq_broker:dlq_broker --fs-discover
+# доп воркер для реализации dlq
+dlq_broker = AioPikaBroker(
+    url="amqp://guest:guest@localhost:5672",
+    queue_name="dlq_taskiq_queue",  # назначаем основную очередь для этого воркера
+    exchange_name="dlq_taskiq_exchange",  # назначаем основной обменник для этого воркера
+    declare_queues=True,
+    persistent=True,
+    queue_durable=True,
+    exchange_durable=True,
+    reconnect_on_fail=True,
+    prefetch_count=20
 )
