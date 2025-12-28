ba4c8c7370d1faf59302f41a08c44517b081751f
Макс <rumyantsev1m0s1@gmail.com>
2025-12-28 22:52:59 +0300

    ts's cookin' me

--- app/core/lifespan.py
+++ app/core/lifespan.py
@@ -3,13 +3,11 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
-from app.core.taskiq_broker import setup_broker
+from app.core.taskiq_broker import broker
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI) -> AsyncGenerator[None, None]:
-    broker = await setup_broker()
-
     if not broker.is_worker_process:
         await broker.startup()
     app.state.broker = broker
