d6704b49e8af5f222b64a1e6fbb2dccafb207ef5
Макс <rumyantsev1m0s1@gmail.com>
2026-01-08 23:16:24 +0300

    proper usage of gevent + deleted eventlet

--- app/core/celery_config.py
+++ app/core/celery_config.py
@@ -9,13 +9,12 @@ app.conf.update(
     broker_url="redis://127.0.0.1:6379/0",
     result_backend="redis://127.0.0.1:6379/1",
 
-    task_serializer='json',
-    accept_content=['json'],
-    result_serializer='json',
-    timezone='Europe/Moscow',
-    worker_pool='gevent',
+    task_serializer="json",
+    accept_content=["json"],
+    result_serializer="json",
+    timezone="Europe/Moscow"
 )
 
 
 if __name__ == '__main__':
-    app.start(['worker', '--loglevel=INFO'])
+    app.start(["worker", "-P", "gevent", "--loglevel=INFO"])
